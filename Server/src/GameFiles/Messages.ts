aa8680e1691f1b9d5fca7a3badc1546eca2bd16c
Sasank-V <sasanklearner@gmail.com>
2024-10-26 12:22:59 +0530

    Please work

--- Server/src/GameFiles/Messages.ts
+++ Server/src/GameFiles/Messages.ts
@@ -3,6 +3,7 @@ export const MOVE = "move";
 export const INVALID_MOVE = "invalid_move";
 export const PLAYER_RESIGN = "player_resign";
 export const GAME_OVER = "game_over";
+export const WEB_STREAM = "web_stream";
 
 //Worker Message Types
 export const VALIDATE_MOVE = "validate_move";
